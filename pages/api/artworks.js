4d193e5a653fdda198c0ef2a5eee31c46d11ef07
siteson828-sketch <siteson828@gmail.com>
2026-04-09 20:20:30 -0400

    Add files via upload

--- pages/api/artworks.js
+++ pages/api/artworks.js
@@ -1,5 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
+export const dynamic = 'force-dynamic';
+
 export default async function handler(req, res) {
   res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate=600');
   try {
@@ -15,15 +17,16 @@ export default async function handler(req, res) {
     if (search) {
       works = await sql`
         SELECT * FROM artworks
-        WHERE commercial_ok = true AND thumb_url IS NOT NULL
-          AND (title ILIKE ${'%' + search + '%'} OR artist ILIKE ${'%' + search + '%'})
+        WHERE commercial_ok = true AND thumb_url IS NOT NULL AND thumb_url != ''
+          AND (title ILIKE ${'%' + search + '%'} OR artist ILIKE ${'%' + search + '%'}
+               OR source ILIKE ${'%' + search + '%'} OR medium ILIKE ${'%' + search + '%'})
         ORDER BY synced_at DESC
         LIMIT ${parseInt(limit)} OFFSET ${parseInt(offset)}
       `;
     } else {
       works = await sql`
         SELECT * FROM artworks
-        WHERE commercial_ok = true AND thumb_url IS NOT NULL
+        WHERE commercial_ok = true AND thumb_url IS NOT NULL AND thumb_url != ''
         ORDER BY synced_at DESC
         LIMIT ${parseInt(limit)} OFFSET ${parseInt(offset)}
       `;
