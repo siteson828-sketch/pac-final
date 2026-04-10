d74528cf2bc1e79ee84f2721ac49bf725c581c93
siteson828-sketch <siteson828@gmail.com>
2026-04-10 16:51:31 -0400

    Add files via upload

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: undefined,
-  experimental: {},
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
-
 module.exports = nextConfig;
